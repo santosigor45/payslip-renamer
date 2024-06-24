915adb45567af4abf4d81c5f4af41032bfd82234
Igor Monteiro <santosigor45@gmail.com>
2024-06-23 21:42:51 -0300

    making last changes

--- app.py
+++ app.py
@@ -74,18 +74,26 @@ selected_output_directory = False
 # GUI setup
 root = ThemedTk(theme="plastik")
 root.title("Renomeador de Holerites")
-file_label = (ttk.Label(root, text="Arquivo PDF:")
-              .grid(row=0, column=0, padx=10, pady=10, sticky="w"))
-file_entry = (ttk.Entry(root, width=50)
-              .grid(row=0, column=1, padx=10, pady=10))
-file_button = (ttk.Button(root, text="Selecionar Arquivo", command=select_file)
-               .grid(row=0, column=2, padx=10, pady=10))
-output_label = (ttk.Label(root, text="Diretório de Saída:")
-                .grid(row=1, column=0, padx=10, pady=10, sticky="w"))
-output_entry = (ttk.Entry(root, width=50)
-                .grid(row=1, column=1, padx=10, pady=10))
-output_button = (ttk.Button(root, text="Selecionar Diretório", command=select_output_directory)
-                 .grid(row=1, column=2, padx=10, pady=10))
-process_button = (ttk.Button(root, text="Processar PDF", command=process_pdf)
-                  .grid(row=2, column=0, columnspan=3, padx=10, pady=10))
+
+file_label = ttk.Label(root, text="Arquivo PDF:")
+file_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+file_entry = ttk.Entry(root, width=50)
+file_entry.grid(row=0, column=1, padx=10, pady=10)
+
+file_button = ttk.Button(root, text="Selecionar Arquivo", command=select_file)
+file_button.grid(row=0, column=2, padx=10, pady=10)
+
+output_label = ttk.Label(root, text="Diretório de Saída:")
+output_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+output_entry = ttk.Entry(root, width=50)
+output_entry.grid(row=1, column=1, padx=10, pady=10)
+
+output_button = ttk.Button(root, text="Selecionar Diretório", command=select_output_directory)
+output_button.grid(row=1, column=2, padx=10, pady=10)
+
+process_button = ttk.Button(root, text="Processar PDF", command=process_pdf)
+process_button.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+
 root.mainloop()
